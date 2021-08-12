4f53df151bb89c6189ff4eb1e656447c0d1cb03d
gateway17 <gateway17ssh@gmail.com>
2021-08-11 23:58:03 -0500

    10th almost DONE./main.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Get amenities of the property """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
